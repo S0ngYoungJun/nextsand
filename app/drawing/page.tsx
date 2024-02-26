ec49273083044868f358bc598ac9345fad2119cb
S0ngyoungjun <clesaaaaaa@gmail.com>
2024-02-26 12:40:23 +0900

    일단마무리

---
+++ app/drawing/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import SandCanvas from '@/app/components/SandCanvas';
+
+export default function Home() {
+  return (
+    <div>
+      <SandCanvas />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
-"use client"
-import SandCanvas from '@/app/components/SandCanvas';
+'use client'
+import { useRouter } from 'next/navigation';
+import styles from "@/app/styles/main.module.scss";
+import Image from 'next/image';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    router.push('/drawing');
+  };
+
   return (
-    <div>
-      <SandCanvas />
+    <div className={styles.world}>
+      <div className={styles.main}>
+        <div className={styles.title}>This is block</div>
+        <button className={styles.go} onClick={handleButtonClick}>
+          지금 시작하기
+        </button>
+      </div>
+      <div className={styles.populer}>
+        <div className={styles.picture}>
+          <Image fill={true} src={`/image/world-screenshot (1).png`} alt={'일단소개'} />
+        </div>
+        <div className={styles.picture}>
+        <Image fill={true} src={`/image/world-screenshot (2).png`} alt={'일단소개'} />
+        </div>
+        <div className={styles.picture}>
+        <Image fill={true} src={`/image/world-screenshot (3).png`} alt={'일단소개'} />
+        </div>
+        <div className={styles.picture}>
+        <Image fill={true} src={`/image/world-screenshot (4).png`} alt={'일단소개'} />
+        </div>
+        <div className={styles.picture}>
+        <Image fill={true} src={`/image/world-screenshot (5).png`} alt={'일단소개'} />
+        </div>
+      </div>
     </div>
   );
 }
